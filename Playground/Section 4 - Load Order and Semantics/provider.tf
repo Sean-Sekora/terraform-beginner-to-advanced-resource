98657d8072d50c0a53253cf2b763996e70c4597b
Sean Sekora <sean@sekora.io>
2024-02-21 15:52:15 -0500

    Section 4

--- Playground/Section 4 - Load Order and Semantics/provider.tf
+++ Playground/Section 4 - Load Order and Semantics/provider.tf
@@ -1,8 +1,8 @@
-variable aws_key {}
-variable aws_secret {}
+variable "aws_key" {}
+variable "aws_secret" {}
 
 provider "aws" {
-  region     = "us-east-1"
+  region     = "us-west-2"
   access_key = var.aws_key
   secret_key = var.aws_secret
 }
