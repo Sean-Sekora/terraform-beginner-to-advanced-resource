3312dc86d6e1942dfc676da4d764c6c5abc56c75
Sean Sekora <sean@sekora.io>
2024-02-25 09:29:52 -0500

    Section 4

--- Playground/Section 4/For Each/main.tf
+++ Playground/Section 4/For Each/main.tf
@@ -1,4 +1,14 @@
-resource "aws_iam_user" "iam" {
-  for_each = toset( ["0", "1", "2", "3"] )
-  name     = "user-${each.key}"
+#resource "aws_iam_user" "iam" {
+#  for_each = toset( ["0", "1", "2", "3"] )
+#  name     = "user-${each.key}"
+#}
+
+resource "aws_instance" "udemy" {
+  ami      = data.aws_ami.app_ami.id
+  for_each = {
+    key1 = "t2.micro"
+    key2 = "t2.small"
+  }
+  instance_type = each.value
+  key_name      = each.key
 }
