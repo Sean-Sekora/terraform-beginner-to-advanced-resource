e9c276d1743b2f54dcf7b5f3dfaeb4fe6b069e48
Sean Sekora <sean@sekora.io>
2024-02-28 10:04:45 -0500

    Section 8

--- Playground/Section 8/Sensitive Parameter/backend.tf
+++ Playground/Section 8/Sensitive Parameter/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region         = "us-east-1"
     bucket         = "sekora-udemy-terraform-state"
-    key            = "section-8/assume-role/terraform.tfstate"
+    key            = "section-8/sensitive-parameter/terraform.tfstate"
     dynamodb_table = "sekora-udemy-terraform-lock"
   }
 }
---
+++ Playground/Section 8/Sensitive Parameter/provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  profile = "udemy"
+  region  = "us-east-1"
+}
