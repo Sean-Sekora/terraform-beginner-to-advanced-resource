b251747f8e3732d01ac300e1a5a6f6f350bb8764
Sean Sekora <sean@sekora.io>
2024-02-28 10:02:51 -0500

    Section 8

--- Playground/Section 8/Terrafrom and Assume Role/main.tf
+++ Playground/Section 8/Terrafrom and Assume Role/main.tf
@@ -1,4 +1,3 @@
-resource "aws_instance" "udemy_east" {
-  ami           = data.aws_ami.app_ami_us_east_1.id
-  instance_type = "t2.micro"
+resource "aws_eip" "eip" {
+    vpc = true
 }
--- Playground/Section 8/Terrafrom and Assume Role/provider.tf
+++ Playground/Section 8/Terrafrom and Assume Role/provider.tf
@@ -1,4 +1,8 @@
 provider "aws" {
+  profile = "udemy_assume_role"
   region  = "us-east-1"
-  profile = "udemy"
+  assume_role {
+    role_arn = "arn:aws:iam::284442860920:role/udemy_assume_role"
+    session_name = "udemy_assume_role"
+  }
 }
